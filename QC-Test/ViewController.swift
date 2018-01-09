e052222189aa1812c20c49b5553d453d52be4e26
Axel Roest <github@phlux.us>
2018-01-09 08:53:35 +0000

    Initial Commit

---
+++ QC-Test/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  QC-Test
+//
+//  Created by Axel Roest on 09/01/2018.
+//  Copyright © 2018 wtf. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
