53d6aca8551b74daa197239359e9f0ee4bc3e528
Axel Roest <github@phlux.us>
2018-01-09 09:15:52 +0000

    First Commit

--- QC-Test/ViewController.swift
+++ QC-Test/ViewController.swift
@@ -7,8 +7,21 @@
 //
 
 import Cocoa
+import Quartz
 
 class ViewController: NSViewController {
+    @IBOutlet weak var quartzView: QCView!
+    
+    override func awakeFromNib() {
+        if let path = Bundle.main.path(forResource: "Telan Cube Replicator", ofType: "qtz") {
+            self.quartzView.loadComposition(fromFile: path)
+            self.quartzView.startRendering()
+        }
+        if let win = self.view.window {
+            win.animationBehavior = .utilityWindow
+        }
+        super.awakeFromNib()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +29,11 @@ class ViewController: NSViewController {
         // Do any additional setup after loading the view.
     }
 
+    override func viewWillDisappear() {
+        self.quartzView.stopRendering()
+        self.quartzView.unloadComposition()
+        super.viewWillDisappear()
+    }
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
